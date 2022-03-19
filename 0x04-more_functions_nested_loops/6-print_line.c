c17afde0bf0d347396e169078049c605369cd8b1
Azeez Razaq Java <52239151+abrotechit@users.noreply.github.com>
2022-03-19 08:16:02 +0100

    6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,16 +1,23 @@
-#include "stdio.h"
+#include "main.h"
+
 /**
- *print_line - to print line in the terminal
- *@n: no. of times to print
- *Return: always 0
+ * print_line - draws a straight line in the terminal
+ * @n: number of times the character _ should be printed
  */
 void print_line(int n)
 {
-int i = 0;
-while (i < n && n > 0)
-{
-_putchar('_');
-i++;
-}
-_putchar('\n');
+	if (n <= 0)
+	{
+		_putchar('\n');
+	} else
+	{
+		int i;
+
+		for (i = 1; i <= n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+
 }
